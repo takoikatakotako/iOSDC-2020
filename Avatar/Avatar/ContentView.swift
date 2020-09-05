4e510dac20d6676c5ad1bae0fa3179b9b3a6d32c
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2020-09-05 15:18:47 +0900

    first commit

---
+++ Avatar/Avatar/ContentView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image("")
+            Image("")
+            HStack {
+                Button(action: {
+                    
+                }) {
+                    Text("xxx")
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

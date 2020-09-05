30d7924bdbfcee4b77cfea85eaebcebc54aa0914
takoikatakotako <snorlax.chemist.and.jazz@gmail.com>
2020-09-05 15:31:41 +0900

    画像を追加した

--- Avatar/Avatar/ContentView.swift
+++ Avatar/Avatar/ContentView.swift
@@ -3,8 +3,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Image("")
-            Image("")
+            Image("background")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+            Image("sd-normal")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 200)
+
             HStack {
                 Button(action: {
                     
